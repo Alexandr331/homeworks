c35de859d51161fb741105ef1a1a33bae822d19a
lu <alexandryumanov0@gmail.com>
2019-04-08 16:42:39 +0300

    delete_hw_03

--- HW_03/Alexandr_Yumanov/alexandr_yumanov_hw_03_t_01.rb
+++
@@ -1,7 +0,0 @@
-def task_01(str)
-  result = ''
-  str.each_line do |line|
-    return line.chomp if line.downcase.include? 'error'
-  end
-  result
-end
--- HW_03/Alexandr_Yumanov/alexandr_yumanov_hw_03_t_04.rb
+++
@@ -1,3 +0,0 @@
-def task_04(str)
-  str.each_char.map(&:to_i).sum
-end
