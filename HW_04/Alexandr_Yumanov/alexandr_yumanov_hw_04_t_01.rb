061107421b220cd2df0ebdd4b2d5ac0fa0936dfb
lu <alexandryumanov0@gmail.com>
2019-04-09 17:11:38 +0300

    fix_hw_04

--- HW_04/Alexandr_Yumanov/alexandr_yumanov_hw_04_t_01.rb
+++ HW_04/Alexandr_Yumanov/alexandr_yumanov_hw_04_t_01.rb
@@ -1,7 +1,8 @@
 def task_4_1(num)
   return 0 if num.zero?
 
-  num.times.each_with_object([1]) do |n, obj|
+  num.times.each_with_object([1, 1]) do |n, obj|
     obj << obj[n - 1] + obj[n - 2] if n >= 1 && n <= 1000
   end
 end
+puts task_4_1(5)
--- HW_04/Alexandr_Yumanov/alexandr_yumanov_hw_04_t_03.rb
+++ HW_04/Alexandr_Yumanov/alexandr_yumanov_hw_04_t_03.rb
@@ -1,3 +1,3 @@
-def task_4_3(_array)
-  [].flatten!.sort { |y, x| x <=> y }
+def task_4_3(array)
+  array.flatten!.sort { |y, x| x <=> y }
 end
